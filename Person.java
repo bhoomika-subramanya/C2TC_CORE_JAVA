c309e8b4207e27712c86f893c6d5eb2e5086835e
Bhoomika Subramanya <bhoomikasubramanya3172004@gmail.com>
2025-08-03 17:44:04 +0530

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,64 @@
+package tns_encapsulation;
+
+public class Person {
+	private int age;
+	private String name;
+	private int income;
+	private String gender;
+	 
+	
+	
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public int getIncome() {
+		return income;
+	}
+
+
+
+	public void setIncome(int income) {
+		this.income = income;
+	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Person [age=" + age + ", name=" + name + ", income=" + income + ", gender=" + gender + "]";
+	}
+
+}
