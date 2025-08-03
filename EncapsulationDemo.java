1b5473945ce774b0f7aa1fbdd07c6865e816fa0a
Bhoomika Subramanya <bhoomikasubramanya3172004@gmail.com>
2025-08-03 17:57:37 +0530

    Create EncapsulationDemo.java

---
+++ EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package tns_encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		OopsDemoCtc obj = new OopsDemoCtc();
+		obj.setAge(21);
+		obj.setName("Apoorva");
+		obj.setEmail("appu123@gmail.com");
+System.out.println(obj);
+
+
+	}
+}
