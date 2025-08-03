b36acd00e6dcea752b1f8815c08878dfc072eef1
Bhoomika Subramanya <bhoomikasubramanya3172004@gmail.com>
2025-08-03 17:37:32 +0530

    Create FirstProgram1.java

---
+++ FirstProgram1.java
@@ -0,0 +1,13 @@
+package c2tc_package;
+
+public class FirstProgram1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("hello world");
+ System.out.println("hello C2TC");
+ System.out.println("hello JSS");
+
+	}
+
+}
