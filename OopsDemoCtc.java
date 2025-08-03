ddcba3b67e02309b5efc4cece71bb8d29827ab20
Bhoomika Subramanya <bhoomikasubramanya3172004@gmail.com>
2025-08-03 17:56:52 +0530

    Create OopsDemoCtc.java

---
+++ OopsDemoCtc.java
@@ -0,0 +1,52 @@
+package tns_encapsulation;
+
+public class OopsDemoCtc {
+	
+	 private  int age;
+	private  String name;
+	private  String email;
+	
+	
+	
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "OopsDemoCtc [age=" + age + ", name=" + name + ", email=" + email + "]";
+	}
+
+}
