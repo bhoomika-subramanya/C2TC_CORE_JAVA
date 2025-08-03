aecc5574102300ccddeddfb124f50a3c56321bb3
Bhoomika Subramanya <bhoomikasubramanya3172004@gmail.com>
2025-08-03 17:42:55 +0530

    Create PersonDEMO.java

---
+++ PersonDEMO.java
@@ -0,0 +1,16 @@
+package tns_encapsulation;
+
+public class PersonDEMO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person per = new Person();
+		per.setName("Akash");
+		per.setGender("Male");
+		per.setAge(20);
+		per.setIncome(30000);
+		
+        System.out.println(per);
+	}
+
+}
