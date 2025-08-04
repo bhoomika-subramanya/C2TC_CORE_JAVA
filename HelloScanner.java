856536ecd61a5e505ddf7f1f2b8986ed222463a0
Bhoomika Subramanya <bhoomikasubramanya3172004@gmail.com>
2025-08-04 11:42:39 +0530

    Update and rename Hello.java to HelloScanner.java

--- HelloScanner.java
+++ HelloScanner.java
@@ -1,6 +1,6 @@
 package com.tnsif.scanner;
  import java.util.*;
-public class Hello {
+public class HelloScanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
