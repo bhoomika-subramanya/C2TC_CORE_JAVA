62c118f4f05a80146012c6eaa4bfb004b5dafc24
Bhoomika Subramanya <bhoomikasubramanya3172004@gmail.com>
2025-08-04 11:43:45 +0530

    Create PersonConstructor.java

---
+++ PersonConstructor.java
@@ -0,0 +1,48 @@
+package com.tnsif.constructor;
+
+public class PersonConstructor {
+
+	 private String personName;
+	private String personCity;
+	private int personID;
+	
+	
+	public Person() {
+		this.personName="Apoorva R S";
+		this.personCity="Kengeri";
+		this.personID=1;
+	
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Person(String personName, String personCity, int personID) {
+		super();
+		this.personName = personName;
+		this.personCity = personCity;
+		this.personID = personID;
+	}
+
+	public String getPersonName() {
+		return personName;
+	}
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	public String getPersonCity() {
+		return personCity;
+	}
+	public void setPersonCity(String personCity) {
+		this.personCity = personCity;
+	}
+	public int getPersonID() {
+		return personID;
+	}
+	public void setPersonID(int personID) {
+		this.personID = personID;
+	}
+	@Override
+	public String toString() {
+		return "Person [personName=" + personName + ", personCity=" + personCity + ", personID=" + personID + "]";
+	}
+	
+}
